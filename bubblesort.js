404e3c012be0ac553e83a91c55abfd6404e89698
Sahm Samarghandi <sahm.samarghandi@gmail.com>
2022-08-16 17:57:37 -0700

    added some bubble sort optimizations that turn it to O(nlogn)

--- bubblesort.js
+++ bubblesort.js
@@ -1,12 +1,19 @@
 function sort(unsortedArray){
     const N = unsortedArray.length;
+    let swapped = false;
     for (let x = 0; x < N-1; x++){
+        swapped = false;
         for (let y = 0; y < N-1-x; y++){
             if (unsortedArray[y]>unsortedArray[y+1]){
                 let temp = unsortedArray[y];
                 unsortedArray[y] = unsortedArray[y+1];
                 unsortedArray[y+1] = temp;
+		swapped = true;
             }
+        }
+
+	if(!swapped){
+            break;
         }
     }
     return unsortedArray;
